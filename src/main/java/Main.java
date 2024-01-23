b2ae5241424bc393638426e8e65325531f6a2646
Edem Ablayev <eablaev3@gmail.com>
2024-01-23 18:29:16 +0300

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BonusServise servise = new BonusServise();
+        long bonus = servise.calculate(5_000, true);
+    }
+}
